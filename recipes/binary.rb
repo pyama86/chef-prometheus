7fa2e261d71e4b4789c0539c74473b4f94e9c65f
pyama86 <pyama@pepabo.com>
2020-04-13 21:51:17 +0900

    use put

--- recipes/binary.rb
+++ recipes/binary.rb
@@ -36,4 +36,5 @@ ark dir_name do
   group node['prometheus']['group']
   extension node['prometheus']['file_extension'] unless node['prometheus']['file_extension'].empty?
   not_if "test -e #{Chef::Config['file_cache_path']}/prometheus-#{node['prometheus']['version']}"
+  action :put
 end
